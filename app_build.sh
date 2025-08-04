d7864ab1a487921c46def94348a6f4ccc33d2308
박세영 <casio2978@naver.com>
2025-08-05 05:52:43 +0900

    [Setting] 앱 빌드 쉘 파일 추가

---
+++ app_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 옵션 파싱
+if [[ "$1" == "--clean" ]]; then
+  echo "🧹 Running flutter clean..."
+  flutter clean
+fi
+
+# build_runner 실행
+echo "🔧 Running build_runner..."
+flutter pub run build_runner build --delete-conflicting-outputs
+
+# dart fix 자동 적용
+echo "🧼 Running dart fix..."
+dart fix --apply
+
+echo "✅ Done!"
