6ee7aa0451fe77e6a1ac9cb211471170506274b1
박세영 <casio2978@naver.com>
2025-08-15 03:48:38 +0900

    [Chore] String, Asset const, textStyle 추가

--- lib/presenter/const/string_const.dart
+++ lib/presenter/const/string_const.dart
@@ -2,4 +2,5 @@ abstract final class StringConst {
   static const String home = '홈';
   static const String chat = '채팅';
   static const String my = '마이';
+  static const String todayPartnerMatch = '오늘의 이성 매칭';
 }
--- lib/presenter/theme/app_text.dart
+++ lib/presenter/theme/app_text.dart
@@ -2,39 +2,45 @@ import 'package:flutter/material.dart';
 
 abstract final class AppTextStyle {
   static const b22 = TextStyle(
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
     fontSize: 22,
     height: 1.18,
   );
 
   static const b20 = TextStyle(
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
     fontSize: 20,
     height: 1.20,
   );
 
   static const b18 = TextStyle(
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
     fontSize: 18,
     height: 1.22,
   );
 
   static const b16 = TextStyle(
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
     fontSize: 16,
     height: 1.25,
   );
 
   static const b14 = TextStyle(
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
     fontSize: 14,
     height: 1.14,
   );
 
   static const b12 = TextStyle(
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
     fontSize: 12,
     height: 1.17,
+  );
+
+  static const sb20 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 1.20,
   );
 
   static const m20 = TextStyle(
