2810db8fa30778288c2bfae7a40f81f0b3efdb98
박세영 <casio2978@naver.com>
2025-08-15 02:38:14 +0900

    [feat] App Route 정의 (chat_list, my_info)

--- lib/core/router/route_enum.dart
+++ lib/core/router/route_enum.dart
@@ -1,6 +1,8 @@
 enum AppRoute {
   home(path: '/home', name: 'Home'),
-  login(path: '/login', name: 'Login');
+  login(path: '/login', name: 'Login'),
+  chatList(path: '/chatList', name: 'ChatList'),
+  myInfo(path: '/myInfo', name: 'MyInfo');
 
   const AppRoute({required this.path, required this.name});
 
