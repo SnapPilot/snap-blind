398898b80f25efb38969b8d783089c9857f808d9
박세영 <casio2978@naver.com>
2025-08-19 00:16:16 +0900

    [Feat] 에러 로그 강화

--- lib/presenter/base/base_bloc.dart
+++ lib/presenter/base/base_bloc.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:snap_blind/core/logger/app_logger.dart';
+import 'package:snap_blind/presenter/base/base_state.dart';
 
 import '../../core/di/di.dart';
 
-abstract base class BaseBloc<E, S> extends Bloc<E, S> {
+abstract base class BaseBloc<E, S extends BaseState> extends Bloc<E, S> {
   BaseBloc(super.initialState);
 
   @override
@@ -13,6 +14,12 @@ abstract base class BaseBloc<E, S> extends Bloc<E, S> {
 
     if (kDebugMode) {
       final AppLogger logger = getIt<AppLogger>();
+
+      if (transition.nextState.stateType == BaseStateType.failure) {
+        logger.error('[Bloc] Transition: $transition');
+        return;
+      }
+
       logger.info('[Bloc] Transition: $transition');
     }
   }
