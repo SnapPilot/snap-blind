7adbb568275efef69e4367697ef4e29df8a58db3
박세영 <casio2978@naver.com>
2025-08-19 00:05:11 +0900

    [Design] String Const 정의

--- lib/presenter/const/string_const.dart
+++ lib/presenter/const/string_const.dart
@@ -4,4 +4,6 @@ abstract final class StringConst {
   static const String my = '마이';
   static const String todayPartnerMatch = '오늘의 이성 매칭';
   static const String startChat = '채팅하기';
+  static const String loremIpsum =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 }
