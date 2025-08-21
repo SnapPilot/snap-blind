1fd661965d68615d801b056dbc77af685313014d
박세영 <casio2978@naver.com>
2025-08-22 00:36:12 +0900

    [Chore] 파일명 수정

--- lib/data/recipe/source/remote/dummy/dummy_recipe_data_source.dart
+++ lib/data/recipe/source/remote/dummy/dummy_recipe_data_source.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 import 'package:snap_blind/core/error/result.dart';
-import 'package:snap_blind/core/network/result_call_adapter.dart';
+import 'package:snap_blind/core/network/call_adapter.dart';
 import 'package:snap_blind/data/recipe/dto/remote/dummy/dummy_recipe_res_dto.dart';
 
 part 'dummy_recipe_data_source.g.dart';
