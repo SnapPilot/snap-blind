61dd540bfa517b04fe671b298b4fd9e705fb95d7
박세영 <casio2978@naver.com>
2025-08-24 20:44:01 +0900

    [Design] 앱 디바이더 속성 추가

--- lib/presenter/widget/app_divider.dart
+++ lib/presenter/widget/app_divider.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:snap_blind/presenter/theme/app_colors.dart';
 
 final class AppDivider extends StatelessWidget {
-  const AppDivider({super.key});
+  const AppDivider({
+    super.key,
+    this.thickness,
+    this.color = AppColors.cGray200,
+  });
+
+  final double? thickness;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const Divider(color: AppColors.cGray200, height: 1);
+    return Divider(color: color, thickness: thickness);
   }
 }
