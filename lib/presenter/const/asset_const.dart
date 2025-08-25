e99ee58a4c6023b45473cd996da310bd52e1fa02
박세영 <casio2978@naver.com>
2025-08-26 00:25:48 +0900

    [Chore] String Const 제거

--- lib/presenter/const/asset_const.dart
+++ lib/presenter/const/asset_const.dart
@@ -2,5 +2,4 @@ abstract final class AssetConst {
   static const String appLogoPath = 'assets/svg/app_logo.svg';
   static const String kakaoLoginBtnPath = 'assets/png/kakao_login_btn.png';
   static const String appIconPath = 'assets/svg/app_icon.svg';
-  static const String avatarIconPath = 'assets/png/avatar.png';
 }
