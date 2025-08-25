cd14ee4f78bdc45b116c55ad9c9dbd652000422d
박세영 <casio2978@naver.com>
2025-08-25 23:28:18 +0900

    [Design] 라벨 텍스트 폼 필드 위젯

---
+++ lib/presenter/widget/labeled_text_form_field.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:snap_blind/presenter/theme/app_colors.dart';
+import 'package:snap_blind/presenter/theme/app_text_style.dart';
+
+final class LabeledTextFormField extends StatelessWidget {
+  const LabeledTextFormField({
+    super.key,
+    required this.label,
+    this.onChanged,
+    required this.hintText,
+    this.keyboardType,
+    this.isDense = false,
+  });
+
+  final String label;
+  final TextInputType? keyboardType;
+  final ValueChanged<String>? onChanged;
+  final String hintText;
+  final bool isDense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: AppTextStyle.sb12.copyWith(color: AppColors.cGray600),
+        ),
+        TextFormField(
+          keyboardType: TextInputType.number,
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            hintText: hintText,
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: AppColors.primary),
+            ),
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: AppColors.cGray300),
+            ),
+            hintStyle: AppTextStyle.r18.copyWith(color: AppColors.cGray500),
+            isDense: false,
+          ),
+        ),
+      ],
+    );
+  }
+}
