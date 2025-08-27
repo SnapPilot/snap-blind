a1427176e367d0c02cc7d1517e9fe276dd9f5bd2
박세영 <casio2978@naver.com>
2025-08-28 01:04:10 +0900

    [Chore] 불필요한 모듈 삭제

--- lib/core/di/register_module.dart
+++ lib/core/di/register_module.dart
@@ -1,12 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
-import 'package:snap_blind/data/recipe/source/remote/dummy/dummy_recipe_data_source.dart';
 
 @module
 abstract class RegisterModule {
   @lazySingleton
   Dio dio() => Dio();
-
-  @lazySingleton
-  DummyRecipeDataSource recipeApi(Dio dio) => DummyRecipeDataSource(dio);
 }
