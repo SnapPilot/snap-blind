5c8eb98f9a6d01ae1292d709d6ef1703fed1830c
박세영 <casio2978@naver.com>
2025-09-03 01:16:39 +0900

    feat: authEvent, authState,

--- lib/presenter/bloc/auth/auth_state.dart
+++ lib/presenter/bloc/auth/auth_state.dart
@@ -1,33 +1,54 @@
 import 'package:flutter/foundation.dart';
 import 'package:snap_blind/domain/auth/entity/auth_token_entity.dart';
+import 'package:snap_blind/domain/auth/entity/user_entity.dart';
 import 'package:snap_blind/presenter/base/base_state.dart';
 
 @immutable
 base class AuthState extends BaseState {
-  const AuthState({super.stateType, super.errorMessage, this.tokenEntity});
+  const AuthState({
+    super.stateType,
+    super.errorMessage,
+    this.userEntity,
+    this.authTokenEntity,
+  });
 
-  final AuthTokenEntity? tokenEntity;
+  final UserEntity? userEntity;
+  final AuthTokenEntity? authTokenEntity;
 
   @override
-  List<Object?> get props => [...super.props, tokenEntity];
+  List<Object?> get props => [...super.props, userEntity, authTokenEntity];
 
   AuthState copyWith({
-    AuthTokenEntity? tokenEntity,
+    UserEntity? userEntity,
+    AuthTokenEntity? authTokenEntity,
     BaseStateType? stateType,
     String? errorMessage,
   }) {
     return AuthState(
       stateType: stateType ?? this.stateType,
       errorMessage: errorMessage ?? this.errorMessage,
-      tokenEntity: tokenEntity ?? this.tokenEntity,
+      userEntity: userEntity ?? this.userEntity,
+      authTokenEntity: authTokenEntity ?? this.authTokenEntity,
     );
   }
 }
 
 @immutable
+final class KaKaoLoginSuccessState extends AuthState {
+  const KaKaoLoginSuccessState(
+    UserEntity userEntity,
+    AuthTokenEntity authTokenEntity,
+  ) : super(
+        stateType: BaseStateType.success,
+        userEntity: userEntity,
+        authTokenEntity: authTokenEntity,
+      );
+}
+
+@immutable
 final class LoginSuccessState extends AuthState {
-  const LoginSuccessState(AuthTokenEntity token)
-    : super(stateType: BaseStateType.success, tokenEntity: token);
+  const LoginSuccessState(UserEntity userEntity)
+    : super(stateType: BaseStateType.success, userEntity: userEntity);
 }
 
 @immutable
