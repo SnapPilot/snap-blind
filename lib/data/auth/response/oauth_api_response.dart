98b16d479cb7cbd323fc0279a8bd84e7eb7c2d58
박세영 <casio2978@naver.com>
2025-09-03 01:21:23 +0900

    feat: oAuth api response 구현

---
+++ lib/data/auth/response/oauth_api_response.dart
@@ -0,0 +1,9 @@
+import 'package:snap_blind/domain/auth/entity/auth_token_entity.dart';
+import 'package:snap_blind/domain/auth/entity/user_entity.dart';
+
+final class OAuthApiResponse {
+  OAuthApiResponse({required this.authTokenEntity, required this.userEntity});
+
+  final AuthTokenEntity authTokenEntity;
+  final UserEntity userEntity;
+}
